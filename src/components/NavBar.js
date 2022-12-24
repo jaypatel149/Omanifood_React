a85e8a9aff179ba27838e064bb7c200af396235b
Your Name <jaiprakashpatel18@navgurukul.org>
2022-12-24 13:35:24 +0530

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./All.css";
+
+const NavBar = () => {
+  return (
+    <header className="header " style={{backgroundColor:'#99960b'}}>
+      <img className="logo" alt="Omnifood logo" src="./img/omnifood-logo.png" />
+      <nav className="main-nav">
+        <ul className="main-nav-list">
+          <li>
+            <a className="main-nav-link" href="#how">
+              How it works
+            </a>
+          </li>
+          <li>
+            <a className="main-nav-link" href="#meals">
+              Meals
+            </a>
+          </li>
+          <li>
+            <a className="main-nav-link" href="#testimonial">
+              Testiomonias
+            </a>
+          </li>
+          <li>
+            <a className="main-nav-link" href="#pricing">
+              Princing
+            </a>
+          </li>
+          <li>
+            <a className="main-nav-link nav-cta" href="#cta">
+              Try for free
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <button className="btn-mobile-nav">
+        <ion-icon className="icon-mobile-nav" name="menu-outline"></ion-icon>
+        <ion-icon className="icon-mobile-nav" name="close-outline"></ion-icon>
+      </button>
+    </header>
+  );
+};
+
+export default NavBar;
